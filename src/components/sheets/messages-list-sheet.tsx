3f595214123c4c3ff675c054a358977502341254
Christian Alares <christian.alares@gmail.com>
2024-03-22 08:33:04 +0100

    comment out code

--- src/components/sheets/messages-list-sheet.tsx
+++ src/components/sheets/messages-list-sheet.tsx
@@ -5,69 +5,71 @@ import { cn } from '@/lib/utils'
 import { useMessagesListSheetStore } from '@/store'
 import { format } from 'date-fns'
 
-type MenuItemProps = {
-  message: Message
-}
+// type MenuItemProps = {
+//   message: Message
+// }
 
-const MessageItem = ({ message }: MenuItemProps) => {
-  const { messageId, setMessageId } = useSelectedTicket()
-  const isActive = messageId === message.id
+export const MessageItem = (/* { message }: MenuItemProps */) => {
+  return null
+  // const { messageId, setMessageId } = useSelectedTicket()
+  // const isActive = messageId === message.id
 
-  const { close } = useMessagesListSheetStore()
+  // const { close } = useMessagesListSheetStore()
 
-  const handleOnClick = () => {
-    setMessageId(message.id)
-    close()
-  }
+  // const handleOnClick = () => {
+  //   setMessageId(message.id)
+  //   close()
+  // }
 
-  return (
-    <li>
-      <button
-        type="button"
-        // href={`/inbox/${message.id}`}
-        onClick={handleOnClick}
-        className={cn('w-full text-left block p-4 border rounded-md hover:border-border-hover', {
-          'bg-foreground/5': isActive,
-        })}
-      >
-        <p className="flex items-center justify-between">
-          <span className="flex items-center gap-2">
-            {!message.read && <span className="rounded-full bg-blue-500 size-2" />}
-            {message.from.name}
-          </span>
-          <time className="text-muted text-xs" dateTime={message.date.toISOString()}>
-            {format(message.date, 'MMM d - HH:mm')}
-          </time>
-        </p>
-        <p className="mt-2">
-          <span className="line-clamp-1">{message.subject}</span>
-          <span className="mt-2 text-muted line-clamp-2">{message.body}</span>
-        </p>
-      </button>
-    </li>
-  )
+  // return (
+  //   <li>
+  //     <button
+  //       type="button"
+  //       // href={`/inbox/${message.id}`}
+  //       onClick={handleOnClick}
+  //       className={cn('w-full text-left block p-4 border rounded-md hover:border-border-hover', {
+  //         'bg-foreground/5': isActive,
+  //       })}
+  //     >
+  //       <p className="flex items-center justify-between">
+  //         <span className="flex items-center gap-2">
+  //           {!message.read && <span className="rounded-full bg-blue-500 size-2" />}
+  //           {message.from.name}
+  //         </span>
+  //         <time className="text-muted text-xs" dateTime={message.date.toISOString()}>
+  //           {format(message.date, 'MMM d - HH:mm')}
+  //         </time>
+  //       </p>
+  //       <p className="mt-2">
+  //         <span className="line-clamp-1">{message.subject}</span>
+  //         <span className="mt-2 text-muted line-clamp-2">{message.body}</span>
+  //       </p>
+  //     </button>
+  //   </li>
+  // )
 }
 
 export const MessagesListSheet = () => {
-  const { isOpen, close } = useMessagesListSheetStore()
+  return null
+  // const { isOpen, close } = useMessagesListSheetStore()
 
-  return (
-    <Sheet open={isOpen} onOpenChange={close}>
-      <SheetContent side="left" className="w-[85vw] sm:max-w-xl overflow-auto">
-        <SheetHeader>
-          <SheetTitle className="text-left">Messages</SheetTitle>
-        </SheetHeader>
+  // return (
+  //   <Sheet open={isOpen} onOpenChange={close}>
+  //     <SheetContent side="left" className="w-[85vw] sm:max-w-xl overflow-auto">
+  //       <SheetHeader>
+  //         <SheetTitle className="text-left">Messages</SheetTitle>
+  //       </SheetHeader>
 
-        <div className="mt-8">
-          <nav>
-            <ul className="flex flex-col gap-2 ">
-              {messages.map((message) => (
-                <MessageItem key={message.id} message={message} />
-              ))}
-            </ul>
-          </nav>
-        </div>
-      </SheetContent>
-    </Sheet>
-  )
+  //       <div className="mt-8">
+  //         <nav>
+  //           <ul className="flex flex-col gap-2 ">
+  //             {messages.map((message) => (
+  //               <MessageItem key={message.id} message={message} />
+  //             ))}
+  //           </ul>
+  //         </nav>
+  //       </div>
+  //     </SheetContent>
+  //   </Sheet>
+  // )
 }
