fd36a6283af5b71ca1b45b41b11d90a194c918b0
Christian Alares <christian.alares@gmail.com>
2024-03-23 20:24:58 +0100

    edit middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,12 @@ import { type NextRequest, NextResponse } from 'next/server'
 export async function middleware(req: NextRequest) {
   const allowedIps = await get<string[]>('whitelisted-ips')
 
-  if (process.env.NODE_ENV !== 'development' && !allowedIps?.includes(req.ip ?? '')) {
+  // Redirect to /closed if the user is not allowed to access the site
+  if (
+    process.env.NODE_ENV !== 'development' &&
+    req.nextUrl.pathname !== '/closed' &&
+    !allowedIps?.includes(req.ip ?? '')
+  ) {
     return Response.redirect(new URL('/closed', req.url))
   }
 
@@ -12,5 +17,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|favicon.ico|closed).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
