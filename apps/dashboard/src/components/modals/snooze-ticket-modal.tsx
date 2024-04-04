307fe4ca4e0b88f3c6ae758626c5074caea088a1
Christian Alares <christian.alares@gmail.com>
2024-04-04 21:31:00 +0200

    update pushmodal

--- apps/dashboard/src/components/modals/snooze-ticket-modal.tsx
+++ apps/dashboard/src/components/modals/snooze-ticket-modal.tsx
@@ -1,15 +1,15 @@
 import { SnoozeTicketForm } from '@/components/forms/snooze-ticket-form'
-import { ModalDescription, ModalHeader, ModalTitle } from '@seventy-seven/ui/modal'
+import { Modal, ModalDescription, ModalHeader, ModalTitle } from '@seventy-seven/ui/modal'
 
 export const SnoozeTicketModal = () => {
   return (
-    <div className="">
+    <Modal>
       <ModalHeader>
         <ModalTitle>Snooze Ticket</ModalTitle>
         <ModalDescription>When the time has expired this will automatically be put back in your inbox</ModalDescription>
       </ModalHeader>
 
       <SnoozeTicketForm />
-    </div>
+    </Modal>
   )
 }
