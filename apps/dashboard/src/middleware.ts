c4253d7b2c85200ffec64b38d6bf9401b8f05501
Christian Alares <christian.alares@gmail.com>
2024-04-20 18:47:26 +0200

    Refactor middleware for access control based on email whitelist and route protection

--- apps/dashboard/src/middleware.ts
+++ apps/dashboard/src/middleware.ts
@@ -2,31 +2,45 @@ import { createClient } from '@seventy-seven/supabase/clients/middleware'
 import { get } from '@vercel/edge-config'
 import { type NextRequest, NextResponse } from 'next/server'
 
-const OPEN_PATHS = ['/', '/closed']
+const OPEN_PATHS = ['/closed']
 
 export async function middleware(req: NextRequest) {
-  // Get allowed IP addresses from the KV store
-  const allowedIps = await get<string[]>('whitelisted-ips')
-
-  // Redirect to /closed if the user's IP is not whitelisted
-  if (
-    process.env.NODE_ENV !== 'development' &&
-    req.nextUrl.pathname !== '/closed' &&
-    !allowedIps?.includes(req.ip ?? '')
-  ) {
-    return Response.redirect(new URL('/closed', req.url))
-  }
+  const IS_DEV = process.env.NODE_ENV === 'development'
+  const isProtectedRoute = !OPEN_PATHS.includes(req.nextUrl.pathname)
+  const allowedEmails = (await get<string[]>('whitelisted-emails')) ?? []
+  const isHomePage = req.nextUrl.pathname === '/'
 
   const supabase = createClient(req)
   const { data } = await supabase.auth.getUser()
 
-  if (/\/invites\/.+/.test(req.nextUrl.pathname) && !data.user) {
-    return Response.redirect(new URL(`/?returnTo=${req.nextUrl.pathname}`, req.url))
+  const isUserWhitelisted = allowedEmails.includes(data.user?.email)
+
+  if (IS_DEV && data.user) {
+    return NextResponse.next()
   }
 
-  // If we're on a protected path and the user is not logged in, redirect to /
-  if (!OPEN_PATHS.includes(req.nextUrl.pathname) && !data.user) {
-    return Response.redirect(new URL('/', req.url))
+  if (isHomePage) {
+    if (!data.user) {
+      return NextResponse.next()
+    }
+
+    if (data.user && !isUserWhitelisted) {
+      return Response.redirect(new URL('/closed', req.url))
+    }
+
+    return NextResponse.next()
+  }
+
+  if (isProtectedRoute) {
+    if (data.user && isUserWhitelisted) {
+      return NextResponse.next()
+    }
+
+    if (data.user && !isUserWhitelisted) {
+      return Response.redirect(new URL('/closed', req.url))
+    }
+
+    return Response.redirect(new URL(`/?returnTo=${req.nextUrl.pathname}`, req.url))
   }
 
   return NextResponse.next()
