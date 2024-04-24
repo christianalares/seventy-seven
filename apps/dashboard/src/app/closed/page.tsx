8541007526cdd43e564119695cc69946497367ae
Christian Alares <christian.alares@gmail.com>
2024-04-24 20:00:09 +0200

    remove commented code

--- apps/dashboard/src/app/closed/page.tsx
+++ apps/dashboard/src/app/closed/page.tsx
@@ -45,14 +45,6 @@ const ClosedPage = async () => {
         </div>
       </div>
     </div>
-    // <div className="flex flex-col justify-center items-center h-full text-center px-4">
-    //   <span className="text-9xl opacity-40">⏳</span>
-    //   <p className="mt-4 text-2xl">
-    //     77 is not yet open to the public.
-    //     <br />
-    //     Please check back later
-    //   </p>
-    // </div>
   )
 }
 
