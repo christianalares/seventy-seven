d7e4226001c5274f98beaf42c278c46ffc360dcd
Christian Alares <christian.alares@gmail.com>
2024-04-25 21:57:49 +0200

    remove unused code

--- apps/dashboard/src/app/(app)/@authed/(root)/page.tsx
+++ apps/dashboard/src/app/(app)/@authed/(root)/page.tsx
@@ -1,6 +1,4 @@
 import { usersQueries } from '@/queries/users'
-// biome-ignore lint/correctness/noUnusedImports: <explanation>
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@seventy-seven/ui/card'
 import { redirect } from 'next/navigation'
 
 const AuthorizedPage = async () => {
@@ -11,25 +9,6 @@ const AuthorizedPage = async () => {
   }
 
   return null
-
-  // return (
-  //   <div className="m-8">
-  //     <Card className="">
-  //       <CardHeader>
-  //         <CardTitle>Whats new?</CardTitle>
-  //         <CardDescription>You have 3 incoming tickets</CardDescription>
-  //       </CardHeader>
-
-  //       <CardContent>
-  //         <ul>
-  //           <li>Ticket 1</li>
-  //           <li>Ticket 2</li>
-  //           <li>Ticket 3</li>
-  //         </ul>
-  //       </CardContent>
-  //     </Card>
-  //   </div>
-  // )
 }
 
 export default AuthorizedPage
