10c2e11177faa2ecfad08f5f8aec7e78dc0d523f
Christian Alares <christian.alares@gmail.com>
2024-06-17 14:30:55 +0200

    remove prisma optimize

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -4,24 +4,7 @@ const config = {
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      return config
-    }
-
-    config.resolve = config.resolve ?? {};
-    config.resolve.fallback = config.resolve.fallback ?? {};
-
-    // these are not available in the browser:
-    config.resolve.fallback.async_hooks = false;
-    config.resolve.fallback.fs = false;
-    config.resolve.fallback.child_process = false;
-    config.resolve.fallback['stream/promises'] = false;
-    config.resolve.fallback.net = false;
-
-    return config;
-  },
+  }
 }
 
 export default config
--- packages/orm/src/prisma.ts
+++ packages/orm/src/prisma.ts
@@ -1,13 +1,8 @@
 import { PrismaClient } from '@prisma/client/edge'
 export * from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { withOptimize } from '@prisma/extension-optimize'
 
 const prismaClientSingleton = () => {
-  if (process.env.NODE_ENV === 'development') {
-    return new PrismaClient().$extends(withAccelerate()).$extends(withOptimize())
-  }
-
   return new PrismaClient().$extends(withAccelerate())
 }
 
