81382fed06d59c9cce26f3e663016387de531760
Christian Alares <christian.alares@gmail.com>
2025-01-14 22:04:45 +0100

    upgrade prisma, move user prefetch to settings layout

--- apps/dashboard/src/app/(app)/settings/layout.tsx
+++ apps/dashboard/src/app/(app)/settings/layout.tsx
@@ -1,4 +1,5 @@
 import { SettingsSubNav } from '@/components/settings-sub-nav'
+import { HydrateClient, trpc } from '@/trpc/server'
 
 export const dynamic = 'force-dynamic'
 
@@ -7,14 +8,19 @@ type Props = {
 }
 
 const SettingsLayout = ({ children }: Props) => {
+  trpc.users.me.prefetch()
+
   return (
-    <main className="overflow-y-scroll">
-      <div className="m-4 md:m-8">
-        <h1 className="text-2xl">Team settings</h1>
-        <SettingsSubNav className="mt-8" />
-        <div className="mt-8">{children}</div>
-      </div>
-    </main>
+    <HydrateClient>
+      <main className="overflow-y-scroll">
+        <div className="m-4 md:m-8">
+          <h1 className="text-2xl">Team settings</h1>
+          <SettingsSubNav className="mt-8" />
+
+          <div className="mt-8">{children}</div>
+        </div>
+      </main>
+    </HydrateClient>
   )
 }
 
--- apps/dashboard/src/app/(app)/settings/page.tsx
+++ apps/dashboard/src/app/(app)/settings/page.tsx
@@ -1,18 +1,13 @@
 import { UpdateTeamAvatarForm } from '@/components/forms/update-team-avatar-form'
 import { UpdateTeamNameForm } from '@/components/forms/update-team-name-form'
 import { PageWrapper } from '@/components/page-wrapper'
-import { HydrateClient, trpc } from '@/trpc/server'
 
 const SettingsPage = () => {
-  trpc.users.me.prefetch()
-
   return (
-    <HydrateClient>
-      <PageWrapper className="space-y-4">
-        <UpdateTeamNameForm />
-        <UpdateTeamAvatarForm />
-      </PageWrapper>
-    </HydrateClient>
+    <PageWrapper className="space-y-4">
+      <UpdateTeamNameForm />
+      <UpdateTeamAvatarForm />
+    </PageWrapper>
   )
 }
 
